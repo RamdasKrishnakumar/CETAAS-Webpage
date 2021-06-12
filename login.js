daa8574ec42e76fb605fea528a9518be3fbd10e6
RamdasKrishnakumar <kramdas1996@gmail.com>
2021-06-13 06:12:01 +0800

    update login

--- login.js
+++ login.js
@@ -4,10 +4,11 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+    random = "72b7415b8c5f53358db8b9517376cd95"
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === CryptoJS.AES.decrypt('U2FsdGVkX18TVI3FN91NkaOhsBokyGRqRT+PcMpoPx0=',random).toString(CryptoJS.enc.Utf8) && password === CryptoJS.AES.decrypt('U2FsdGVkX1/TbuZKkYFpOe01p+oLf+CGgl3YL1O54sA=',random).toString(CryptoJS.enc.Utf8)) {
         alert("You have successfully logged in.");
         window.location.replace("google_drive docs.html");
     } else {
