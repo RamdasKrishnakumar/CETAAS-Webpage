c0f40aec7135b36edd45b7a06e932353ba1996af
RamdasKrishnakumar <kramdas1996@gmail.com>
2021-06-20 10:31:59 +0800

    updated php

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "mdsubhan.53@gmail.com";
+        $recipient = "kramdas1996@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
